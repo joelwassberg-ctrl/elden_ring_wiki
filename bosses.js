b128ce208da1134dc63df00f0f311088833d4c0d
Joel Wassberg <joel.wassberg@skola.taby.se>
2026-02-19 13:09:05 +0100

    Test bosses API fetch funktion

---
+++ bosses.js
@@ -0,0 +1,33 @@
+const url = "https://eldenring.fanapis.com/api/bosses";
+
+async function fetchBosses() {
+    try{
+const response = await fetch(url);
+const data = await response.json();
+console.log(data);
+    const boss = data.data;
+
+    if (!boss) {
+      console.log("Boss not found!");
+      return;
+    }
+
+    const container = document.getElementById("boss_container");
+    container.innerHTML = "";
+    
+    boss.forEach(boss => {
+        const img = document.createElement("img");
+        img.src = boss.image;
+        img.alt = boss.name;
+
+        const name = document.createElement("p");
+        name.textContent = boss.name;
+
+        container.appendChild(img);
+        container.appendChild(name);
+    });
+   } catch(err){
+    console.error("Error fetching bosses:", err);
+  }
+}
+fetchBosses();
