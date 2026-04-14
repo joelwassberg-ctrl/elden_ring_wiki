1a2144029f4ad9c712e090f62f8f6e9a650378ee
Joel Wassberg <joel.wassberg@skola.taby.se>
2026-04-14 16:40:23 +0200

    ignore

--- Main/main.js
+++ Main/main.js
@@ -1,11 +1,11 @@
 const boxes = document.querySelectorAll(".box");
 boxes.forEach(box => {
     const name = box.dataset.name;
-    box.style.setProperty("background-image", `url(../images/${name}.webp)`);
+    box.style.setProperty("background-image", `url(../Images/${name}.webp)`);
     box.addEventListener("mouseover", () => {
-        box.style.setProperty("background-image", `url(../images/${name}_hover.webp)`);
+        box.style.setProperty("background-image", `url(../Images/${name}_hover.webp)`);
     });
         box.addEventListener("mouseleave", () => {
-        box.style.setProperty("background-image", `url(../images/${name}.webp)`);
+        box.style.setProperty("background-image", `url(../Images/${name}.webp)`);
     });
 })
